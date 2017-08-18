ed3e4ffb6d8f57d7f271a4765216d089a7437faa
waiyanphyohein <waiyanphyohein@gmail.com>
2017-08-18 02:58:03 -0400

    Changes to main.bundle.js.

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -3,7 +3,7 @@
 var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     var _monthlyRate = 0;
     if (rate) {
-        monthlyRate = rate / 100 / 12;
+        _monthlyRate = rate / 100 / 12;
     }
     var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
     return monthlyPayment;
--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-let calculateMonthlyPayment = function (principal, years, rate) {
-    let _monthlyRate = 0;
+let calculateMonthlyPayment = function(principal, years, rate) {
+    let monthlyRate = 0;
     if (rate) {
         monthlyRate = rate / 100 / 12;
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
     return monthlyPayment;
 };
-
 document.getElementById('calcBtn').addEventListener('click', function () {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
